a966500672fbd9f39980792d5563107810c27f4d
Avi Saini <avisaini@Avis-MacBook-Air.local>
2023-08-02 19:33:50 +0530

    landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import FrameTwentyOnePage from "./FrameTwentyOne/index";
+const Home = () => {
+  return (
+  <FrameTwentyOnePage/>
+  );
+};
+export default Home;
